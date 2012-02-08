31eb5f4b05d06fa24fd8f003896e498af831b37a
Adi Eyal <adi@burgercom.co.za>
2012-02-08 21:50:59 +0200

    ADded conversion script

---
+++ code/convert_to_pdf.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from pbs import inkscape
+
+for path, _, files in os.walk("output"):
+    for f in files:
+        if f.endswith(".svg"):
+            in_path = os.path.join(path, f)
+            out_path = os.path.join(path, f.replace(".svg", ".pdf"))
+            print "Converting: %s > %s" % (in_path, out_path)
+            inkscape("-A", "/tmp/crap.pdf", in_path )
+
+            
