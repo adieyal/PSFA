eaf5af63b52bb57a75e75878b56823d32b7b70fb
Adi Eyal <adi@burgercom.co.za>
2012-02-13 16:35:24 +0200

    calculate output path dynamically

--- code/convert_to_pdf.py
+++ code/convert_to_pdf.py
@@ -2,7 +2,10 @@ import os
 import sys
 from pbs import inkscape
 
-for path, _, files in os.walk("output"):
+code_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+project_root = os.path.join(code_dir, os.path.pardir)
+output_dir = os.path.join(project_root, "output")
+for path, _, files in os.walk(output_dir):
     for f in files:
         if f.endswith(".svg"):
             in_path = os.path.join(path, f)
