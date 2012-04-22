46632cd8f44f15fd2560b8c4c12d2fd586932c72
Adi Eyal <adi@burgercom.co.za>
2012-04-22 22:41:17 +0200

    Moved schooldata out into a separate file

--- code/generate.py
+++ code/generate.py
@@ -17,316 +17,7 @@ from processutils import xmlutils
 from specialint import SpecialInt
 import constants
 from statswriter import StatsWriter
-
-school_map = {}
-menus = {}
-
-class AvgSchoolData(object):
-    def __init__(self, school_datas=None):
-        self._school_datas = school_datas or []
-
-    def add_observation(self, school_data):
-        self._school_datas.append(school_data)
-
-    def __getattr__(self, key):
-        if not key in self.__dict__:
-            return stats.mean([getattr(s, key) for s in self._school_datas])
-        else:
-            return self.__dict__[key]
-        
-
-class SchoolData(object):
-    field_types = {
-        "yes_is_1" : [
-            "C1", "C2", "C4", "C5", "C6", "C7", "C8", "C9",
-            "D1", "D3", "D4", "D5", 
-            "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "E11", "E12", "E13",
-            "F1", "F3", "F4", "F5", "F6", "F7", "F8",
-        ],
-        "no_is_1" : [
-            "C3", "C10", "C11",
-            "D6",
-        ],
-        "is_date" : [
-            "A5",
-        ],
-        "is_time" : [
-            "D7a", "D7b"
-        ],
-        "is_int" : [
-            "B3", "B6",
-            "G1", "G2", "G3", "G4",
-        ],
-        "is_float" : [
-            "D8b", "D9b", "D10b", "D11b", "D12b", "D13b",
-        ],
-    }
-
-    def __init__(self, data_row, headers, xls_datemode):
-        self.data_row = data_row
-        # TODO I don't like passing the datemode into the class but I'm not sure it makes sense
-        # to pre-parse the data outside of the class either
-        self.xls_datemode = xls_datemode
-        self.headers = headers
-        self._data = dict(zip(self.headers, self.data_row))
-
-    def __getattr__(self, key):
-        try:
-            val = self._get_col_value(key)
-            if key in SchoolData.field_types["yes_is_1"]:
-                return SpecialInt(self.yes_is_1(val))
-            elif key in SchoolData.field_types["no_is_1"]:
-                return SpecialInt(self.no_is_1(val))
-            elif key in SchoolData.field_types["is_date"]:
-                return strptime(val.strip(), "%d.%m.%Y")
-            elif key in SchoolData.field_types["is_time"]:
-                return self.parse_time(val)
-            elif key in SchoolData.field_types["is_int"]:
-                return SpecialInt(self.parse_int(val))
-            elif key in SchoolData.field_types["is_float"]:
-                return float(val)
-            return val
-        except:
-            print "Could not understand the value in col: %s for %s" % (key, self.name)
-            sys.exit(1)
-            #import traceback
-            #traceback.print_exc()
-        
-    def _get_col_value(self, prefix):
-        return self._data[prefix]
-
-    def yes_is_1(self, val):
-        if self.is_no_response(val):
-            if self.voluteers_interviewed:
-                return self.yes_is_1("no")
-            return None
-        return 1 if val.lower().strip() == "yes" else 0
-
-    def no_is_1(self, val):
-        if self.is_no_response(val):
-            if self.voluteers_interviewed:
-                return self.no_is_1("no")
-            return None
-        return 1 if val.lower().strip() == "no" else 0
-
-    def parse_int(self, val):
-        if self.is_no_response(val):
-            return None
-        return int(val)
-
-    def parse_time(self, val):
-        if self.is_no_response(val):
-            return SpecialInt(None)
-        return parse_willa_time(val, self.xls_datemode)
-
-    def is_no_response(self, val):
-        return val in ["Didn't answer", "", "None"]
-
-    def score_rating(self, x):
-        if x == None: return x
-
-        if x >= 5: return 1
-        return 0
-
-    @property
-    def visit(self):
-        return self.A7
-
-    @property
-    def is_cooking_school(self):
-        return self.B7 == "Cooking"
-
-    @property
-    def voluteers_interviewed(self):
-        return "Volunteers (VOL)" in self.A10
-
-    @property
-    def name(self):
-        return school_map[self.school_number]["schoolname"]
-
-    @property
-    def school_number(self):
-        return int(self.A3)
-
-    @property
-    def school_type(self):
-        return school_map[self.school_number]["school_type"]
-
-    @property
-    def visit_date(self):
-        return self.A5
-
-    @property
-    def all_stock(self):
-        if self.is_cooking_school:
-            all_stock = [
-                ("Pilchards", "C12"), 
-                ("Rice", "C13"), 
-                ("Curry Mince", "C14"), 
-                ("Savoury Mince", "C15"),
-                ("Samp", "C16"), 
-                ("Sugar beans", "C17"), 
-                ("Brown lentils", "C18")]
-        else:
-            all_stock = [
-                ("Jam", "C24"), 
-                ("Peanut butter", "C25")
-            ]
-        return all_stock
-
-    @property
-    def total_score(self):
-        delivery = self.meal_delivery_score
-        safety = self.hygiene_score
-        stock = self.stock_score
-        staff = self.staff_score
-
-        return (delivery + safety + stock + staff) / 40.
-
-    @property
-    def meal_served_on_time(self):
-        time_1030 = datetime.time(10, 30)
-        return self.D7a < time_1030
-
-    @property
-    def ind_meal_served_on_time(self):
-        if self.D7a == None:
-            return SpecialInt(None)
-        return 2 if self.meal_served_on_time else 0
-
-    @property
-    def meal_served_efficiently(self):
-        if self.D7a == None or self.D7b == None:
-            return SpecialInt(None)
-        diff = self.D7b - self.D7a
-        return diff.minutes < 30
-
-    @property
-    def ind_meal_served_efficiently(self):
-        return 1 if self.meal_served_efficiently else 0
-
-    @property
-    def food_in_good_condition(self):
-        return (self.C5 + self.C6 + self.C7 + self.C8) * 0.5
-
-    @property
-    def menu(self):
-        menu_name = "%s %s schools" % (self.school_type, "cooking" if self.is_cooking_school else "non-cooking")
-        return menus[menu_name]
-
-    @property
-    def food_deviations(self):
-        val_total_fed = self.B3
-        val_date_of_visit = self.visit_date
-        val_day_of_visit = constants.day_of_week[val_date_of_visit.weekday()]
-        if val_day_of_visit == "Saturday": val_day_of_visit = "Friday"
-        if val_day_of_visit == "Sunday": val_day_of_visit = "Monday"
-
-        ranges = [] 
-        for field_idx in range(8, 14):
-            ingredient_type_field = "D%da" % field_idx
-            ingredient_qty_field = "D%db" % field_idx
-
-            ingredient = getattr(self, ingredient_type_field)
-
-            # Don't look at ingredients that are not on the menu
-            if ingredient in self.menu:
-                ingredient_pack_qty = constants.ingredient_qtys[ingredient]
-                amount_needed = val_total_fed * self.menu[ingredient][val_day_of_visit]
-                amount_served = getattr(self, ingredient_qty_field) * ingredient_pack_qty
-                if amount_needed == 0:
-                    range_perc = 0
-                else:
-                    range_perc = abs(amount_served - amount_needed) / amount_needed
-
-                # Don't include deviations where a quantity served is 0
-                if amount_served > 0:
-                    ranges.append((ingredient, range_perc))
-        return ranges
-
-    @property
-    def ind_food_deviations(self):
-        ranges = self.food_deviations 
-        ranges_below_10 = [range_perc for range_perc in ranges if range_perc < 0.1]
-        if len(ranges_below_10) == len(ranges):
-            pt_ranges = 2
-        elif len(ranges_below_10) >= 3:
-            pt_ranges = 1
-        else:
-            pt_ranges = 0
-        return pt_ranges
-        
-    @property
-    def meal_delivery_score(self):
-        return float(sum([
-            self.D1, self.D3, self.D4, self.D5, self.D6, 
-            self.ind_meal_served_on_time, self.ind_meal_served_efficiently, self.ind_food_deviations
-        ]))
-
-    @property
-    def hygiene_score(self):
-        return sum([
-            self.E2, self.E3, self.E4, self.E5,
-            self.E7, self.E8, self.E9, 
-            self.E12, self.E13, 
-            self.E10 * 0.5, self.E11 * 0.5,
-            1 if (self.E5 == 0 and self.E6 == 1) else 0
-        ])
-
-    @property
-    def staples_in_stock(self):
-        all_stock = self.all_stock
-        days_left = self.B6
-        weeks_left = days_left / 5.0
-
-        stock_surpluses = []
-        for (ingredient, indicator) in self.all_stock:
-            available_stock_days = getattr(self, "%sb" % indicator)
-            feeding_days_per_week = sum([1 for x in self.menu[ingredient].values() if x > 0])
-            required_stock_days = feeding_days_per_week * weeks_left
-            surplus_stock_days = available_stock_days - required_stock_days
-            
-            stock_surpluses.append((ingredient, surplus_stock_days >= 0))
-        return all([x[1] for x in stock_surpluses])
-
-    @property
-    def ind_staples_in_stock(self):
-        return 2 if self.staples_in_stock else 0
-
-    @property
-    def ind_G1(self):
-        return self.score_rating(self.G1)
-
-    @property
-    def ind_G2(self):
-        return self.score_rating(self.G1)
-
-    @property
-    def ind_G3(self):
-        return self.score_rating(self.G1)
-
-    @property
-    def ind_G4(self):
-        return self.score_rating(self.G1)
-
-    @property
-    def stock_score(self):
-        return sum([
-            self.C1, self.C2, self.C3, self.C4, self.C9,
-            self.C5 * 0.5, self.C6 * 0.5, self.C7 * 0.5, self.C8 * 0.5,
-            self.C10 * 0.5, self.C11 * 0.5,
-            self.ind_staples_in_stock
-        ])
-        return ind1 + ind2
-
-
-    @property
-    def staff_score(self):
-        return sum([
-            self.F1, self.F3, self.F4, self.F5, self. F6, self.F7,
-            self.score_rating(self.G1), self.score_rating(self.G2), 
-            self.score_rating(self.G3), self.score_rating(self.G4), 
-        ])
+from schooldata import AvgSchoolData, SchoolData
 
 re_qnum = re.compile("^(Timestamp|Verification Sch Number|[A-Z]\d+[a-z]?.).*")
 def extract_header(header):
@@ -339,19 +30,11 @@ def extract_header(header):
     #print header, " IS NONE!!!"
     return None
 
-def parse_willa_time(val, datemode):
-    if val == "9:00 or earlier":
-        return Time(9, 0, 0)
-    elif val == "12:00 or later":
-        return Time(12, 0, 0)
-    date_tuple = xlrd.xldate_as_tuple(val, datemode)[3:]
-    return Time(*date_tuple)
-            
-
-def load_data(filename, visit, calc_year, calc_month):
+def load_data(filename, visit, calc_year, calc_month, context):
     xls = xlrd.open_workbook(filename)
     sheet = xls.sheets()[0]
     data = defaultdict(list, {})
+    school_map = context["school_map"]
 
     headers = sheet.row_values(0)
     headers = [extract_header(header) for header in headers]
@@ -363,7 +46,7 @@ def load_data(filename, visit, calc_year, calc_month):
 
         current_visit = d["A7"]
 
-        school_data = SchoolData(data_row, headers, xls.datemode)
+        school_data = SchoolData(context, data_row, headers, xls.datemode)
         # Only collect results from this year and from january until the current month
         if school_data.visit_date.year != calc_year: continue
         if school_data.visit_date.month > calc_month: continue
@@ -379,8 +62,10 @@ def load_data(filename, visit, calc_year, calc_month):
     return data
 
 def load_schooltypes(filename):
+    # each school is either primary or secondary
     xls = xlrd.open_workbook(filename)
     sheet = xls.sheets()[0]
+    school_map = {}
 
     headers = sheet.row_values(0)
     
@@ -391,9 +76,12 @@ def load_schooltypes(filename):
         data["school_type"] = "Primary" if int(data["primary_school"]) == 1 else "Secondary"
         data["score_card"] = int(data["score_card"])
         school_map[school_num] = data
+    return school_map
 
 def load_menu(filename):
+    # load the menus for primary and secondary coooking and non-cooking schools
     xls = xlrd.open_workbook(filename)
+    menus = {}
     for i in range(4):
         sheet = xls.sheets()[i]
 
@@ -407,6 +95,8 @@ def load_menu(filename):
             datum = dict(zip(headers, row[2:]))
             items[ingredient] = datum
         menus[sheet.name] = items
+    return menus
+        
 
 def generate_graph(xml, prefix, val, width=19.407):
     # ensure that ceil of 5 is 6
@@ -640,15 +330,14 @@ def main(args):
         os.mkdir(os.path.join(output_dir, "scorecard"))
         os.mkdir(os.path.join(output_dir, "noscorecard"))
 
-    # each school is either primary or secondary
-    load_schooltypes(os.path.join(resource_dir, "school_type.xls"))
-    # load the menus for primary and secondary coooking and non-cooking schools
-    load_menu(os.path.join(resource_dir, "menu.xls"))
+    context = {}
+    context["school_map"] = load_schooltypes(os.path.join(resource_dir, "school_type.xls"))
+    context["menu"] = load_menu(os.path.join(resource_dir, "menu.xls"))
 
     template_xml = open(os.path.join(resource_dir, "scorecard2.svg")).read().decode("utf-8")
 
     # load all visit data
-    all_data = load_data(filename, visit, calc_year, calc_month)
+    all_data = load_data(filename, visit, calc_year, calc_month, context)
 
     all_visit_data = all_data["current_visit"]
     for i, school in enumerate(all_visit_data):
@@ -657,7 +346,7 @@ def main(args):
         school_xml = template_xml
         school_xml = render_scorecard(all_data, school, school_xml)
 
-        output_path = os.path.join(output_dir, "%s" % ("scorecard" if school_map[school.school_number]["score_card"] == 1 else "noscorecard"))
+        output_path = os.path.join(output_dir, "%s" % ("scorecard" if context["school_map"][school.school_number]["score_card"] == 1 else "noscorecard"))
         output_file = "%d.svg" % school.school_number
         f = open(os.path.join(output_path, output_file), "w")
         f.write(school_xml.encode("utf-8"))
